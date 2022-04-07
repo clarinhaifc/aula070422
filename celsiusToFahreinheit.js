a3f78343a8c30cfed74238b82617fc1d220bf251
clarinhaifc <Claraeu15@outlook.com>
2022-04-07 11:08:39 -0300

    1° versao

---
+++ celsiusToFahreinheit.js
@@ -0,0 +1,7 @@
+/* celsius-to-fahrenheit.js */
+const converters = require ('./converters.js');
+
+const celsiusInput = process.argv[2]; // Pega a 3ª entrada de uma lista de argumentos
+const fahrenheitValue = converters.celsiusToFahrenheit(celsiusInput);
+
+console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
